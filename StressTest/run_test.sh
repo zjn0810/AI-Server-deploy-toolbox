904138b6c11b12be6f9c46f70daf0c4a3f4991cd
zjn0810 <2443464529@qq.com>
2026-03-15 21:22:05 +0800

    fix run.sh

--- StressTest/run_test.sh
+++ StressTest/run_test.sh
@@ -40,14 +40,13 @@ select_tests() {
     if [ -t 1 ] && command -v dialog >/dev/null 2>&1; then
         choices=$(dialog --clear --title "Burn-in Tests" --checklist \
         "Select tests to run (SPACE toggle, ENTER confirm)" 20 78 10 \
-        "GPU BURN" "gpu_burn" OFF \
+        "GPU_BURN" "gpu_burn" OFF \
         "FIELDIAG_L1" "fieldiag --level1 --no_bmc" OFF \
         "FIELDIAG_L2" "fieldiag --level2 --no_bmc" OFF \
         "CPU" "stress-ng cpu" OFF \
         "MEM" "stress-ng mem" OFF \
         "DISK" "fio randrw" OFF \
         3>&1 1>&2 2>&3)
-
         exit_status=$?
         clear
 
@@ -60,7 +59,7 @@ select_tests() {
 
         for item in $choices; do
             case "$item" in
-                "\"GPU\"") RUN_GPU=1 ;;
+                "\"GPU_BURN\"") RUN_GPU=1 ;;
                 "\"FIELDIAG_L1\"") RUN_FIELDIAG_L1=1 ;;
                 "\"FIELDIAG_L2\"") RUN_FIELDIAG_L2=1 ;;
                 "\"CPU\"") RUN_CPU=1 ;;
@@ -72,7 +71,7 @@ select_tests() {
     elif [ -t 1 ] && command -v whiptail >/dev/null 2>&1; then
         choices=$(whiptail --title "Burn-in Tests" --checklist \
         "Select tests to run (SPACE toggle, ENTER confirm)" 20 78 10 \
-        "GPU BURN" "gpu_burn -tc" off \
+        "GPU_BURN" "gpu_burn -tc" off \
         "FIELDIAG_L1" "fieldiag --level1 --no_bmc" off \
         "FIELDIAG_L2" "fieldiag --level2 --no_bmc" off \
         "CPU" "stress-ng cpu" off \
@@ -95,7 +94,7 @@ select_tests() {
 
         for item in $choices; do
             case "$item" in
-                "\"GPU BURN\"") RUN_GPU=1 ;;
+                "\"GPU_BURN\"") RUN_GPU=1 ;;
                 "\"FIELDIAG_L1\"") RUN_FIELDIAG_L1=1 ;;
                 "\"FIELDIAG_L2\"") RUN_FIELDIAG_L2=1 ;;
                 "\"CPU\"") RUN_CPU=1 ;;
@@ -250,11 +249,11 @@ if [ $RUN_DISK -eq 1 ]; then
             --rw=randrw           # 随机读写
 			--rwmixread=70        # 70% 读
 			--bs=4k               # 4KB 块大小
-			--ioengine=libaio     # 异步 IO
+			--ioengine=io_uring   
 			--direct=1            # 绕过操作系统缓存
 			--thread              # 使用线程模式
-			--numjobs=4           # 每个设备 4 个线程 job
-			--iodepth=64          # 队列深度 64
+			--numjobs=8           # 每个设备 4 个线程 job
+			--iodepth=128          # 队列深度 64
 			--randrepeat=0        # 每次随机不同
 			--invalidate=1        # 清空缓存影响
 			--norandommap         # 减少内存占用
